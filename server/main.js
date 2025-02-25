8d54c6096cacbee30bce580b81833ef6d84da8a2
Toby Gore <tobyedwardgore@gmail.com>
2025-02-25 14:16:01 +0000

    wokring end points:
    * GET hazards
    * GET reports
    * POST new report

--- server/main.js
+++ server/main.js
@@ -1,19 +1,19 @@
 "use strict";
+// #region set up
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require('express');
 var lstat = require('fs').lstat;
 var path = require('path');
 // import modules for supabase 
 var supabase_js_1 = require("@supabase/supabase-js");
-// quick hand notation
-var json_js_1 = require("./json.js");
-console.log((0, json_js_1.decodeJson)('{"test": "test"}'));
 // read `.env`
 require('dotenv').config();
 // init app
 var app = express();
 // set port
 var PORT = 3000;
+// needed for post and get requests
+app.use(express.json());
 // connect to database
 var supabaseUrl = 'https://lhbvrjbqokpjbubaanqv.supabase.co';
 var supabaseKey = process.env.SUPABASE_KEY;
@@ -26,29 +26,105 @@ var tables = {
 // sets a default error
 app.listen(PORT, function (error) {
     if (!error) {
-        console.log("Example app listening at http://localhost:".concat(PORT));
+        console.info("Example app listening at http://localhost:".concat(PORT));
     }
     else {
-        console.log(error);
+        console.error(error);
     }
 });
 // sets a home screen of index.html
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
+// #endregion setup
 // a test connection for the sql
-app.get('/get-hazards', function (req, res) {
+app.get('/hazards', function (req, res) {
     tables.hazards.select('*').then(function (data) {
         res.send(data.data);
     });
 });
-app.get('/get-users', function (req, res) {
-    tables.users.select('*').then(function (data) {
+// // app.get('/users', (req, res) => {
+// //     tables.users.select(
+// //         '*'
+// //     ).then((data) => {
+// //         res.send(data.data);
+// //     })
+// // });
+// // app.get('/user-reports/:id', (req, res) => {
+// //     console.log(req.params)
+// //     let userId: string = req.params.id;
+// //     if (userId == null) {
+// //         res.send('provide id'); 
+// //         return;
+// //     }
+// //     userId = userId.toString();
+// //     tables.reports.select(
+// //         `*`
+// //     ).eq(
+// //         'user_id',
+// //         userId
+// //     ).then((data) => {
+// //         res.send(data.data);
+// //     })  
+// // });
+app.get('/reports', function (req, res) {
+    tables.reports.select('*').then(function (data) {
         res.send(data.data);
     });
 });
-app.get('/get-reports', function (req, res) {
-    tables.users.select('*').then(function (data) {
+app.post('/reports', function (req, res) {
+    // console.log(req.body);
+    var _a = req.body, hazard_id = _a.hazard_id, description = _a.description, time_start = _a.time_start, latitude = _a.latitude, longitude = _a.longitude, estimated_duration = _a.estimated_duration, is_planned = _a.is_planned, severity = _a.severity;
+    var user_id = 1;
+    if (!req.body.description === null) {
+        res.json({ error: 'bad request, send data' });
+        return;
+    }
+    try {
+        tables.reports.insert({
+            user_id: user_id,
+            hazard_id: hazard_id,
+            description: description,
+            time_start: time_start,
+            latitude: latitude,
+            longitude: longitude,
+            estimated_duration: estimated_duration,
+            is_planned: is_planned,
+            severity: severity
+        })
+            .then(function (data) {
+            // res.json(data.data);
+            res.json("success");
+        });
+    }
+    catch (e) {
+        console.error(e);
+        res.json("error in catch");
+        // res.json({error: e.message});
+    }
+});
+app.delete('/report/:id', function (req, res) {
+    var reportId = req.params.id;
+    if (reportId == null) {
+        res.send('provide report id');
+        return;
+    }
+    reportId = reportId.toString();
+    tables.reports.delete().eq('id', reportId).then(function (data) {
         res.send(data.data);
     });
 });
+/*
+
+?: GET all reports
+?: POST new report (assume req.body has same fields as database)
+?  GET all hazards
+
+Todo: GET a report by it’s ID
+Todo: GET all report
+
+Optional:
+Todo: UPDATE change status/severity of report reports(description)
+Todo: DELETE report
+
+*/
--- server/main.ts
+++ server/main.ts
@@ -1,3 +1,5 @@
+// #region set up
+
 const express = require('express');
 const { lstat } = require('fs');
 const path = require('path');
@@ -9,8 +11,6 @@ import { createClient } from '@supabase/supabase-js';
 import { decodeJson, encodeJson } from './json.js';
 import { parse } from 'dotenv';
 
-console.log(decodeJson('{"test": "test"}'));
-
 // read `.env`
 require('dotenv').config();
 
@@ -18,6 +18,8 @@ require('dotenv').config();
 const app = express();
 // set port
 const PORT: number = 3000;
+// needed for post and get requests
+app.use(express.json());
 
 // connect to database
 const supabaseUrl: string = 'https://lhbvrjbqokpjbubaanqv.supabase.co';
@@ -32,9 +34,9 @@ const tables = {
 // sets a default error
 app.listen(PORT, (error) => {
     if (!error) {
-        console.log(`Example app listening at http://localhost:${PORT}`);
+        console.info(`Example app listening at http://localhost:${PORT}`);
     } else {
-        console.log(error);
+        console.error(error);
     }
 });
 
@@ -43,36 +45,116 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// #endregion setup
+
 // a test connection for the sql
-app.get('/get-hazards', (req, res) => {
-    tables.hazards.select('*').then((data) => {
+app.get('/hazards', (req, res) => {
+    tables.hazards.select(
+        '*'
+    ).then((data) => {
         res.send(data.data);
     })
 });
 
-app.get('/get-users', (req, res) => {
-    tables.users.select('*').then((data) => {
+// // app.get('/users', (req, res) => {
+// //     tables.users.select(
+// //         '*'
+// //     ).then((data) => {
+// //         res.send(data.data);
+// //     })
+// // });
+
+// // app.get('/user-reports/:id', (req, res) => {
+
+// //     console.log(req.params)
+// //     let userId: string = req.params.id;
+// //     if (userId == null) {
+// //         res.send('provide id'); 
+// //         return;
+// //     }
+// //     userId = userId.toString();
+
+// //     tables.reports.select(
+// //         `*`
+// //     ).eq(
+// //         'user_id',
+// //         userId
+// //     ).then((data) => {
+// //         res.send(data.data);
+// //     })  
+// // });
+
+app.get('/reports', (req, res) => {
+    tables.reports.select(
+        '*'
+    ).then((data) => {
         res.send(data.data);
     })
 });
 
-app.get('/get-reports', (req, res) => {
-    tables.users.select('*').then((data) => {
+app.post('/reports', (req, res) => {
+    // console.log(req.body);
+    
+    const {hazard_id, description, time_start, latitude, longitude, estimated_duration, is_planned, severity} = req.body  
+
+    const user_id = 1;
+
+    if (!req.body.description === null) {
+        res.json({error: 'bad request, send data'})
+        return; 
+    }
+    
+    try {
+        tables.reports.insert({
+            user_id: user_id,
+            hazard_id: hazard_id,
+            description: description,
+            time_start: time_start,
+            latitude: latitude,
+            longitude: longitude,
+            estimated_duration: estimated_duration,
+            is_planned: is_planned,
+            severity: severity
+        })
+        .then((data) => {
+            // res.json(data.data);
+            res.json("success")
+        })
+    } catch (e) {
+        console.error(e);
+
+        res.json("error in catch")
+        // res.json({error: e.message});
+    }
+});
+
+
+app.delete('/report/:id', (req, res) => {
+    let reportId: string = req.params.id;
+    if (reportId == null) {
+        res.send('provide report id');
+        return;
+    }
+    reportId = reportId.toString();
+
+    tables.reports.delete().eq('id', reportId).then((data) => {
         res.send(data.data);
     })
-});
+})
+
 
 /*
 
-GET specific user reports
+?: GET all reports 
+?: POST new report (assume req.body has same fields as database)
+?  GET all hazards
 
-POST report (assume req.body has same fields as database)
+Todo: GET a report by it’s ID
+Todo: GET all report
 
-GET a report by it’s ID
-
-UPDATE change status/severity of report reports(description)
-
-DELETE report
+Optional:
+Todo: UPDATE change status/severity of report reports(description)
+Todo: DELETE report
 
 */
 
